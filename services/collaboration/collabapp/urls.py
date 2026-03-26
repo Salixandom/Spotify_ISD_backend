6d1780def96429baa34b78a00d6ef9e6ca62d87f
Mesbah Uddin Ahamed <mesbah@Mesbahs-MacBook-Pro.local>
2026-03-26 23:03:19 +0600

    Removed collaborationapp from collaboration service, modified core and collabapp

--- services/collaboration/collabapp/urls.py
+++ services/collaboration/collabapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GenerateInviteView, JoinView, CollaboratorListView
+from .views import GenerateInviteView, JoinView, CollaboratorListView, HealthCheckView
 
 urlpatterns = [
+    path("health/", HealthCheckView.as_view()),
     path("<int:playlist_id>/invite/", GenerateInviteView.as_view()),
     path("join/<str:token>/", JoinView.as_view()),
     path("<int:playlist_id>/members/", CollaboratorListView.as_view()),
